68bc6ce85a16dfd4e581823dd85de5999af88eea
Taylor Lovett <tlovett88@gmail.com>
2016-05-07 11:24:42 -0400

    Splash link path

--- page-splash.php
+++ page-splash.php
@@ -7,8 +7,8 @@
 <?php get_header(); ?>
 
 <h1><span>Burnt House</span> Bermuda</h1>
-<h2><a href="/compound">Enter Site</a></h2>
+<h2><a href="<?php echo esc_url( home_url() ); ?>/compound">Enter Site</a></h2>
 
-<a href="/compound"><img class="splash-img" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/src/BH_Gate_3572.jpg'; ?>"></a>
+<a href="<?php echo esc_url( home_url() ); ?>/compound"><img class="splash-img" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/src/BH_Gate_3572.jpg'; ?>"></a>
 
 <?php get_footer(); ?>
