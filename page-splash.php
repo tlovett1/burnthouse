5389217189ea6e38b2b5a5f18ef170eec447a1f3
Taylor Lovett <tlovett88@gmail.com>
2016-09-02 00:44:27 -0400

    Fix splash

--- page-splash.php
+++ page-splash.php
@@ -1,11 +1,14 @@
 <?php
 /**
- * Template Name: Summary
+ * Template Name: Splash
  */
 ?>
 
 <?php get_header(); ?>
 
+<h1><span>Burnt House</span> Bermuda</h1>
+<h2><a href="<?php echo esc_url( home_url() ); ?>/compound">See Full Tour</a> | <a href="<?php echo esc_url( home_url() ); ?>/summary">See 90 Second Summary</a></h2>
 
+<a href="<?php echo esc_url( home_url() ); ?>/compound"><img class="splash-img" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/src/BH_Gate_3572.jpg'; ?>"></a>
 
 <?php get_footer(); ?>
--- page-summary.php
+++ page-summary.php
@@ -1,14 +1,15 @@
 <?php
 /**
- * Template Name: Splash
+ * Template Name: Summary
  */
 ?>
 
 <?php get_header(); ?>
 
-<h1><span>Burnt House</span> Bermuda</h1>
-<h2><a href="<?php echo esc_url( home_url() ); ?>/compound">See Full Tour</a> | <a href="<?php echo esc_url( home_url() ); ?>/summary">See 90 Second Summary</a></h2>
-
-<a href="<?php echo esc_url( home_url() ); ?>/compound"><img class="splash-img" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/src/BH_Gate_3572.jpg'; ?>"></a>
+<div class="summary">
+	<?php if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
+		<?php the_content(); ?>
+	<?php endwhile; endif; ?>
+</div>
 
 <?php get_footer(); ?>
