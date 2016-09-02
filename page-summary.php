e5a84d5a803b32c794f145141805d4319d93141c
Taylor Lovett <tlovett88@gmail.com>
2016-09-02 00:42:27 -0400

    Fix splash

--- page-summary.php
+++ page-summary.php
@@ -1,15 +1,14 @@
 <?php
 /**
- * Template Name: Summary
+ * Template Name: Splash
  */
 ?>
 
 <?php get_header(); ?>
 
-<div class="summary">
-	<?php if ( have_posts() ) : while ( have_posts() ): the_post(); ?>
-		<?php the_content(); ?>
-	<?php endwhile; endif; ?>
-</div>
+<h1><span>Burnt House</span> Bermuda</h1>
+<h2><a href="<?php echo esc_url( home_url() ); ?>/compound">Enter Site</a></h2>
+
+<a href="<?php echo esc_url( home_url() ); ?>/compound"><img class="splash-img" src="<?php echo esc_url( get_template_directory_uri() ) . '/images/src/BH_Gate_3572.jpg'; ?>"></a>
 
 <?php get_footer(); ?>
